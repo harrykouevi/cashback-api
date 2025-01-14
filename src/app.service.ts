5fcf1d634bcf1a631243e588b293cff7d95d0e57
harry.kouevi <harry.kouevi@gmail.com>
2025-01-14 20:36:36 +0000

    update first route

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      name: 'Cashback-API',
+      version: '1.0.0',
+      description: 'Ceci est une API pour gérer les opérations de cashback, y compris les transactions et les utilisateurs.',
+      endpoints: [
+         
+      ]
+     }
   }
 }
